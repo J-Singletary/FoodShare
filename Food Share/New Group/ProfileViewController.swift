13a6872b6fa4ff55b75d26215657440ceb33c28c
Jared <jonjared9800@aol.com>
2020-04-22 16:01:50 -0700

    Added preliminary logging out

---
+++ Food Share/New Group/ProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.swift
+//  Food Share
+//
+//  Created by user170197 on 4/22/20.
+//  Copyright © 2020 Christian Lay-Geng. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func logoutPressed(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError{
+            print("Error signing out: %@", signOutError)
+        }
+    }
+}
